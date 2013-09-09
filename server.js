bae8fe6428cd4afc23f858adb3559e30240d9281
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-09 12:56:10 -0500

    Updated to get collection

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ var http = require('http'),
     mongo = require('mongoskin');
 
 http.createServer(function (req, res) {
-    var connect = function(dbName, next){
-        var db = mongo.db("mongodb://MongoLab-25:Zqy8wu31J5C6E9Dhxf6Q4t2cyf8ckVkS8dWrnCiMOvw-@ds027748.mongolab.com:27748/MongoLab-25/" + dbName, {safe : true});
+    var connect = function(next){
+        var db = mongo.db("mongodb://MongoLab-25:Zqy8wu31J5C6E9Dhxf6Q4t2cyf8ckVkS8dWrnCiMOvw-@ds027748.mongolab.com:27748/MongoLab-25", {safe : true});
         next(db);
     };
 
-    connect("fantasy", function(db) {
+    connect(function(db) {
         var out = [];
 
-        console.log(db);
-
-        db.collectionNames(function(err,collNames){
+        db.collectionNames(function(err, collNames) {
             if (err) {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(JSON.stringify(err));
