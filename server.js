a0b3ff3540d3b05d0f74f3dd7c61dd1f0a13722c
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-08 21:19:10 -0500

    fix code

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ var http = require('http'),
     mongo = require('mongoskin');
 
 http.createServer(function (req, res) {
+    var connect = function(dbName,next){
+        var db = mongo.db(process.env.CUSTOMCONNSTR_MONGOLAB_URI + '/' + dbName, {safe : true});
+        next(db);
+    };
     
-    var db = mongo.db(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
-    var out = [];
+    connect(admin, function(db) {
+        var out = [];
 
-    db.admin.listDatabases(function(err, result){
-        _.each(result.databases,function(item){
-            out.push(item.name);
+        db.admin.listDatabases(function(err, result){
+            _.each(result.databases,function(item){
+                out.push(item.name);
+            });
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('Hello, world!' + JSON.stringify(out));
+            // res.json(out);
         });
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('Hello, world!' + JSON.stringify(out));
-        // res.json(out);
     });
-
-    
     
 }).listen(process.env.PORT || 8080);
