0c9edf4c59b8b8b1e5391cb3c721a92fb13859bb
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-08 19:15:43 -0500

    Try mongo

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http');
+var http = require('http'),
+    mongo = require('mongoskin');
 
 http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    var db = mongo.db(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+    var out = [];
+
+    db.admin.listDatabases(function(err, result){
+        _.each(result.databases,function(item){
+            out.push(item.name);
+        });
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('Hello, world!' + JSON.stringify(out));
+        // res.json(out);
+    });
+
+    
     
 }).listen(process.env.PORT || 8080);
