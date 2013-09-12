758c857f396518db4eb10041e8370dd2f2bdf4ad
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-12 17:51:40 -0500

    try to get access_token

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@
 /*
  * GET home page.
  */
-var OAuth2 = require('oauth').OAuth2;
+var OAuth2 = require('oauth').OAuth;
 
-var oauth = new OAuth2(
+var oauth = new OAuth(
+    'https://api.login.yahoo.com/oauth/v2/get_request_token',
+    'https://api.login.yahoo.com/oauth/v2/request_auth',
     'dj0yJmk9Tzh5WDgxTXBGRHJzJmQ9WVdrOVMxTmxTMkpoTlRRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yMg--',
     'cda4f2845d41c1fe43e380a68d9acf793b387cd3',
-    'https://api.login.yahoo.com/',
+    '1.0'
+    '/',
     'oauth/v2/get_token',
     'oauth/v2/get_request_token'
 );
@@ -17,5 +20,7 @@ exports.index = function(req, res){
 };
 
 exports.authorize = function(req, res) {
-    res.redirect(oauth.getAuthorizeUrl());
+    oauth.getOAuthRequestToken(function() {
+        console.log(req);
+    });
 };
