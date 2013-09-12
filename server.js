ad30fa494755e93e77eec358ea68c4c9a91c9805
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-12 18:30:03 -0500

    add in session

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.cookieSession());
+app.use(express.session({secret: 'coinlaundry' }));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.cookieSession());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
