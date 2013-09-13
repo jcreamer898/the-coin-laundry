636ef434c5090ca1708070251fceb1986907bb72
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-12 22:32:07 -0500

    add moar

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,65 @@
+(function(global) {
+    var app = angular.module('app', []),
+        PROD = !!process.env.CUSTOMCONNSTR_MONGOLAB_URI;
+
+    var apiRoutes = {
+        myTeam: PROD ? '/api/myteam' : '/javascripts/fixtures/myteam.json'
+    };
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/javascripts/views/home.html',
+                controller: 'HomeController'
+            })
+            .when('/myteams', {
+                templateUrl: '/javascripts/views/myteam.html',
+                controller: 'MyTeamsController'
+            })
+            .otherwise({
+                template: '<h1>Not found</h1>'
+            });
+    });
+
+    app.controller('HomeController', function() {
+
+    });
+
+    app.controller('MyTeamsController', function($scope, $http) {
+        $http({
+            method: 'get',
+            url: apiRoutes.myTeam
+        }).success(function(data) {
+            var teamsData = data.fantasy_content.users[0]
+                    .user[1]
+                    .games[0]
+                    .game[1]
+                    .teams,
+                teams = [];
+
+            angular.forEach(teamsData, function(obj) {
+                var teamData = {};
+
+                if (obj.team) {
+                    angular.forEach(obj.team[0], function(kvp) {
+                        if (kvp === Object(kvp)) {
+                            angular.forEach(kvp, function(value, key) {
+                                teamData[key] = value;
+                            });
+                        }
+                    });
+
+                    teams.push(teamData);
+                }
+                
+            });
+
+            console.log(teams);
+            $scope.teams = teams;
+        });
+    });
+
+
+
+    global.app = app;
+}(window));
--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,6 @@ exports.index = function(req, res) {
     res.render('index', { title: 'Express' });    
 };
 
-exports.myteam = function(req, res) {
-    var oa = new OAuth(req.session.oa._requestUrl,
-        req.session.oa._accessUrl,
-        req.session.oa._consumerKey,
-        req.session.oa._consumerSecret,
-        req.session.oa._version,
-        req.session.oa._authorize_callback,
-        req.session.oa._signatureMethod);
-
-    oa.getProtectedResource('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/teams?format=json',
-        'GET',
-        req.session.oauth_access_token,
-        req.session.oauth_access_token_secret, 
-        function(err, data, response) {
-            res.json(JSON.parse(data));
-        });
-};
-
 exports.oauth = function(req, res) {
     var oa = new OAuth(
         'https://api.login.yahoo.com/oauth/v2/get_request_token',
