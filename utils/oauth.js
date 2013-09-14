9849c6a903635247afccdcd6b41da410499c194b
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-13 21:19:57 -0500

    log verifier

--- utils/oauth.js
+++ utils/oauth.js
@@ -36,7 +36,7 @@ exports.checkLogin = function(req, res, next) {
     }
     // Check the token
     else if (req.session.oauth_token && req.session.oauth_access_token && isProd(req)) {
-        console.log('refreshing token', req.session.oauth_token, req.session.oauth_access_token_secret);
+        console.log('refreshing token', req.session.oauth_verifier);
         get(req.session).getOAuthAccessToken(
             req.session.oauth_token, 
             req.session.oauth_token_secret, 
