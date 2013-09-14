ca40990af648116cc9da2799c7dbce8c78e3a9b0
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-09-13 23:55:31 -0500

    Add config

---
+++ config.js
@@ -0,0 +1,9 @@
+exports = {
+    "accessTokenUrl": "https://api.login.yahoo.com/oauth/v2/get_request_token",
+    "requestTokenUrl": "https://api.login.yahoo.com/oauth/v2/get_token",
+    "oauthKey": "dj0yJmk9MjNqVU5LdUh2Z3kxJmQ9WVdrOWJqbEtXbkkxTTJVbWNHbzlNelE1TnpJNE1UWXkmcz1jb25zdW1lcnNlY3JldCZ4PTdh",
+    "oauthSecret": "fddb72a3210b6508fcb5fe685af92281184c16ee",
+    "version": "1.0",
+    "callback": "http://thecoinlaundry.azurewebsites.net/authorize",
+    "encryption": "HMAC-SHA1"
+};
