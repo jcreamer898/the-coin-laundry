4b286f0b9258bc0c1f2d12c184cc45feccce6201
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-07-10 13:51:21 -0500

    Change config

--- utils/oauth.js
+++ utils/oauth.js
@@ -1,44 +1,43 @@
 var OAuth = require('oauth').OAuth,
-    config = require('../config.js'),
-    get,
-    isProd = function(req) {
-        return !~req.get('host').indexOf('local');
-    };
+    config = require('../config.js');
 
-console.log(config);
-
-exports.get = get = function(session) {
-    var oa = (session || {}).oa ? 
-        new OAuth(
-            session.oa._requestUrl,
-            session.oa._accessUrl,
-            session.oa._consumerKey,
-            session.oa._consumerSecret,
-            session.oa._version,
-            session.oa._authorize_callback,
-            session.oa._signatureMethod
-        ) :
-        new OAuth(
-            config.accessTokenUrl,
-            config.requestTokenUrl,
-            config.oauthKey,
-            config.oauthSecret,
-            config.version,
-            config.callback,
-            config.encryption
-        )
-    
-    return oa
+var isProd = function(req) {
+    return !~req.get('host').indexOf('local');
 };
 
-exports.checkLogin = function(req, res, next) {
-    // No token has been created yet
-    if ((!req.session.oauth_access_token && !req.session.timestamp ||
-        Math.round(((new Date() - new Date(req.session.timestamp)) % 86400000) / 3600000) >= 1) && 
-        isProd(req) && !~req.url.indexOf('oauth')) {
-        res.redirect('/oauth');
-    }
-    else {
-        next();
+exports = {
+    get: function(session) {
+        var oa = (session || {}).oa ? 
+            new OAuth(
+                session.oa._requestUrl,
+                session.oa._accessUrl,
+                session.oa._consumerKey,
+                session.oa._consumerSecret,
+                session.oa._version,
+                session.oa._authorize_callback,
+                session.oa._signatureMethod
+            ) :
+            new OAuth(
+                config.accessTokenUrl,
+                config.requestTokenUrl,
+                config.oauthKey,
+                config.oauthSecret,
+                config.version,
+                config.callback,
+                config.encryption
+            )
+        
+        return oa
+    },
+    checkLogin: function(req, res, next) {
+        // No token has been created yet
+        if ((!req.session.oauth_access_token && !req.session.timestamp ||
+            Math.round(((new Date() - new Date(req.session.timestamp)) % 86400000) / 3600000) >= 1) && 
+            isProd(req) && !~req.url.indexOf('oauth')) {
+            res.redirect('/oauth');
+        }
+        else {
+            next();
+        }
     }
 };
