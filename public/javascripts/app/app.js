8461117ee2b036de3f1153cf6b21963bada6a78d
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2014-07-11 17:54:41 -0500

    Formatting fixes:

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,51 +1,51 @@
 define(function(require) {
-    var angular = require("angular"),
-        controllers = require("controllers"),
-        controllers = require("directives"),
-        services = require("services"),
-        ngRoute = require("ngRoute"),
-        app;
+    var angular = require("angular"), app;
+    
+    require("controllers");
+    require("directives");
+    require("services");
+    require("ngRoute");
 
     app = angular.module("app", [
             "ngRoute",
             "coin.controllers",
             "coin.services",
             "coin.directives" ]);
-
+    
     app.config(function($routeProvider) {
         $routeProvider
-            .when('/', {
-                templateUrl: '/javascripts/views/home.html',
-                controller: 'HomeCtrl',
-                controllerAs: 'home'
+            .when("/", {
+                templateUrl: "/javascripts/views/home.html",
+                controller: "HomeCtrl",
+                controllerAs: "home"
             })
-            .when('/rules', {
-                templateUrl: '/javascripts/views/rules.html',
-                controller: 'RulesCtrl',
-                controllerAs: 'rules'
+            .when("/rules", {
+                templateUrl: "/javascripts/views/rules.html",
+                controller: "RulesCtrl",
+                controllerAs: "rules"
             })
-            .when('/history', {
-                templateUrl: '/javascripts/views/history.html'
+            .when("/history", {
+                templateUrl: "/javascripts/views/history.html"
             })
-            .when('/players', {
-                templateUrl: '/javascripts/views/players.html',
-                controller: 'PlayersCtrl',
-                controllerAs: 'players'
+            .when("/players", {
+                templateUrl: "/javascripts/views/players.html",
+                controller: "PlayersCtrl",
+                controllerAs: "players"
             })
-            .when('/teams', {
-                templateUrl: '/javascripts/views/teams.html',
-                controller: 'TeamsCtrl'
+            .when("/teams", {
+                templateUrl: "/javascripts/views/teams.html",
+                controller: "TeamsCtrl"
             })
-            .when('/teams/:id', {
-                templateUrl: '/javascripts/views/team.html',
-                controller: 'TeamCtrl'
+            .when("/teams/:id", {
+                templateUrl: "/javascripts/views/team.html",
+                controller: "TeamCtrl"
             })
-            .when('/sandbox', {
-                templateUrl: 'javascripts/views/sandbox.html',
-                controller: 'SandboxCtrl'
+            .when("/sandbox", {
+                templateUrl: "javascripts/views/sandbox.html",
+                controller: "SandboxCtrl"
             })
             .otherwise({
-                template: '<h1>Not found</h1>'
+                template: "<h1>Not found</h1>"
             });
     });
 
